ce91589bfaac560fde2191c0a3a944a4135f786a
xiaoqing04.chen <xiaoqing04.chen@vipshop.com>
2022-05-27 16:01:54 +0800

    feat: 去掉sourcemap

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "../public",
-  configureWebpack: {
-    devtool: "source-map",
+  configureWebpack: (config) => {
+    delete config.devtool;
   },
 });
